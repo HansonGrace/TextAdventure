6f04edbb7cd10b09c07cb1325a65e33e67940ea6
Grace Hanson <69362728+HansonGrace@users.noreply.github.com>
2025-04-19 01:25:35 -0400

    rename files

--- main/Main.java
+++ main/Main.java
@@ -1,6 +1,6 @@
 import javax.swing.JFrame;
 
-public class textAdventureWindow {
+public class Main{
     public static void main(String[] args) {
         JFrame frame = new JFrame("Hollow Wilds");
         frame.setSize(800, 600); // or any default size you want
