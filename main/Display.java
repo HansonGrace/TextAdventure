157709404e20ce5d90cbc19f0e69345edc71db43
Grace Hanson <hansongrace2004@gmail.com>
2025-05-03 22:04:33 -0400

    Update Display.java

--- main/Display.java
+++ main/Display.java
@@ -1,13 +1,45 @@
 import javax.swing.JFrame;
 import java.awt.Canvas;
 
-public class Display extends Canvas {
+public class Display extends Canvas implements Runnable {
 
     public static final int WIDTH = 800;
     public static final int HEIGHT = 600;
     public static final String TITLE = "NOTITLE pre-alpha 0.01";
 
-    // private Thread
+    private Thread thread;
+    private boolean running = false;
+
+    // if already running dont initialize the game again
+    private void start() {
+        if (running)
+            return;
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+
+        System.out.println("Game started!");
+    }
+
+    public void stop() {
+        if (!running)
+            return;
+        running = false;
+        try {
+            thread.join();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+    }
+
+    public void run() {
+        while (running) {
+
+        }
+    }
 
     public static void main(String[] args) {
         // new object of Display class
@@ -20,6 +52,8 @@ public class Display extends Canvas {
         frame.setResizable(false);
         frame.setVisible(true);
         frame.setSize(WIDTH, HEIGHT);
+
+        game.start();
 
     }
 }
